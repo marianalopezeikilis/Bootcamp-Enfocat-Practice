21a9dfedd87f884cf1a346e8cfdd56b6676d6861
Maru <marianalopezeikilis>
2022-08-22 13:39:35 +0200

    trabajando con node

--- node js/conversiones.js
+++ node js/conversiones.js
@@ -1,10 +1,18 @@
 
-function m2km(x) {
-return x*1.60934;
+function m2km(m) {
+return m*1.60934;
 }
 
-function km2m(x){
-    return x*0.6214;
+function km2m(km){
+    return km*0.6214;
 }
 
-export  {m2km,km2m  }
+function c2f(c){
+    return c*9/5+32;
+}
+
+function f2c(f){
+return f-32*5/9;
+}
+
+export {m2km,km2m,c2f,f2c}
